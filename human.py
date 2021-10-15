07a2609bb0cf17c2336f6ee1454276e77bb0dc11
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-15 14:17:06 -0500

    Created parent Player and child Human and Computer classes and framed them out

---
+++ human.py
@@ -0,0 +1,11 @@
+from player import Player
+
+class Human(Player):
+    def __init__(self, name):
+        super().__init__()
+
+    def choose_gesture(self, gestures):
+        pass
+
+    def choose_name(self):
+        pass
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self):
+        self.wins = []
+        self.current_gesture = ""
+        self.name = ""
